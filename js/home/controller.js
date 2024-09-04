fd7b9a25353e41527b2dd3c855e5e7d428801a7e
DanielMartinPerezdeLeon <damarpele@gmail.com>
2024-09-04 13:35:02 +0200

    change href of the access-the-api button on menu so it loads it automatically from the config.js

--- js/home/controller.js
+++ js/home/controller.js
@@ -19,3 +19,19 @@ if (validatorVersionLabel !== null && validatorVersionLabel !== undefined) {
 	// Insert the comment node right at the start of the head
 	document.head.insertBefore(commentNode, document.head.firstChild);
 }
+
+
+document.addEventListener('DOMContentLoaded', function () {
+    // Check if swaggerURL is defined
+    if (typeof swaggerURL !== 'undefined' && swaggerURL) {
+        // Set the href attribute of the anchor tag
+        const apiLink = document.getElementById('api-link');
+        if (apiLink) {
+            apiLink.href = swaggerURL;
+        } else {
+            console.warn('API link element not found.');
+        }
+    } else {
+        console.error('swaggerURL is not defined in config.js');
+    }
+});
